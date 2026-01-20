796a54bfec69197fe32d4e226cedf08a0725bd85
aayushmaanhooda <aayushmaan162@gmail.com>
2026-01-20 18:57:07 +1100

    testing my website with hits

--- http-request.js
+++ http-request.js
@@ -3,12 +3,41 @@
 // negative case
 // positive case
 import http from 'k6/http'
+import { check } from 'k6';
 
 export const options = {
-    vus: 10,
-    iterations:20
-}
+    vus: 10,           
+    iterations: 20,   
+    thresholds: {
+        http_req_duration: ['p(95)<2000'],  
+        http_req_failed: ['rate<0.1'],     
+    }
+};
 
 export default function(){
-    http.get("https://test.k6.io")
+    const endpoint = 'https://aayushbot-1.onrender.com/chat';
+
+    const payload = JSON.stringify({
+        message: "Helloo how are you??",
+        session_id : 'test-session' + __VU + '-' + __ITER
+    })
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    };
+
+    console.log(`VU ${__VU} - Sending POST to ${endpoint}`);
+
+    const res = http.post(endpoint, payload, { headers: headers });
+    console.log(`VU ${__VU} - Response status: ${res.status}`);
+
+
+   check(res, {
+        'response code is 200': (res) => res.status === 200,
+        'correct endpoint': (res) => res.url === endpoint,
+        'response has answer': (res) => res.json('answer') !== undefined,
+        'response has session_id': (res) => res.json('session_id') !== undefined,
+        'response time < 2000ms': (res) => res.timings.duration < 2000,
+    });
 }
